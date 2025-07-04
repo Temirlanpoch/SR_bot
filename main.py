fe0ad6ec8bb22aec34fb09e1b88f05d96576e058
Temirlanpoch <88177409+Temirlanpoch@users.noreply.github.com>
2025-07-04 19:44:34 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,37 @@
 import asyncio
-import os
-from dotenv import load_dotenv
-from tele_utils import bot, dp, start_bot, stop_bot, uptime_router
+from tele_utils import bot, dp, start_bot, stop_bot
 from lead_handler import check_and_handle_leads
+
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from datetime import datetime
-
-load_dotenv()
-
-scheduler = AsyncIOScheduler()
-scheduler.start()
+from apscheduler.triggers.cron import CronTrigger
 
 
-async def periodic_check():
+async def periodic_lead_check():
     while True:
         await check_and_handle_leads()
-        await asyncio.sleep(30)
+        await asyncio.sleep(3)  # Проверка каждые 3 секунды
+
 
 async def main():
     await start_bot()
-    asyncio.create_task(periodic_check())
-    await dp.start_polling(bot)
+    scheduler = AsyncIOScheduler()
+
+    # Планировщик для ежедневного отчета
+    scheduler.add_job(
+        check_and_handle_leads,
+        CronTrigger(hour=8, minute=0),  # Ежедневно в 08:00
+        id="daily_report"
+    )
+
+    scheduler.start()
+    try:
+        await periodic_lead_check()
+    finally:
+        await stop_bot()
+
 
 if __name__ == "__main__":
     try:
         asyncio.run(main())
     except (KeyboardInterrupt, SystemExit):
-        asyncio.run(stop_bot())
+        print("Бот остановлен.")
